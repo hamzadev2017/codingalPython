35660700e89b3f45c08a2a1e09a549d3e0465ca6
hamzadev2017 <wahlamuhammadhamza@gmail.com>
2024-11-30 14:37:48 +0500

    Calculate sqrt
    It takes input from user and calculate square root and print it.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# codingalPython
-Repo for submission of work for codingal assignments
+
---
+++ squareroot.py
@@ -0,0 +1,5 @@
+import math
+
+sq = int(input("write any number(no alphabet): "))
+val = math.sqrt(sq)
+print ("Squareroot is: {}".format(val))
