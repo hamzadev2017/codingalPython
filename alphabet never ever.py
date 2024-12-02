7cef55936f3e2cbda1d0e34a70a816873f640d8d
hamzadev2017 <wahlamuhammadhamza@gmail.com>
2024-12-02 21:04:57 +0500

    Create alphabet never ever.py
    this program tells kids about all the alphabet letters

---
+++ alphabet never ever.py
@@ -0,0 +1,58 @@
+boom = input("write any character: ")
+if boom == 'A':
+    print("alphabet")
+elif boom == 'b':
+    print("alphabet")
+elif boom == 'c':
+    print("alphabet")
+elif boom == 'd':
+    print("alphabet")
+elif boom == 'e':
+    print("alphabet")
+elif boom == 'f':
+    print("alphabet")
+elif boom == 'g':
+    print("alphabet")
+elif boom == 'h':
+    print("alphabet")
+elif boom == 'i':
+    print("alphabet")
+elif boom == 'j':
+    print("alphabet")
+elif boom == 'k':
+    print("alphabet")
+elif boom == 'l':
+    print("alphabet")
+elif boom == 'm':
+    print("alphabet")
+elif boom == 'n':
+    print("alphabet")
+elif boom == 'o':
+    print("alphabet")
+elif boom == 'p':
+    print("alphabet")
+elif boom == 'q':
+    print("alphabet")
+elif boom == 'r':
+    print("alphabet")
+elif boom == 's':
+    print("alphabet")
+elif boom == 't':
+    print("alphabet")
+elif boom == 'u':
+    print("alphabet")
+elif boom == 'v':
+    print("alphabet")
+elif boom == 'w':
+    print("alphabet")
+elif boom == 'x':
+    print("alphabet")
+elif boom == 'y':
+    print("alphabet")
+elif boom == 'z':
+    print("alphabet")
+elif boom >= 'A' and boom <= 'Z':
+    print("alphabet")
+else:
+    print("alphabet never ever")
+
