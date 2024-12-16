8671405491fa109aae59e7d6daa5b78ade307f71
hamzadev2017 <wahlamuhammadhamza@gmail.com>
2024-12-16 17:22:04 +0500

    Create Number-swap.py
    it sets and shows us the correct position of three values

---
+++ Number-swap.py
@@ -0,0 +1,12 @@
+threear = input("type any  number : ")
+oner = input("type any number : ")
+twoar = input("type any number : ")
+
+fourar = oner
+oner = twoar
+twoar = threear
+threear = fourar
+
+print("After changing, number one is ", oner)
+print("After changing, number two is ", twoar)
+print("After changing, number three is ", threear)
