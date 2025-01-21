f9da0adf75ac12693308d90c1e79ef391d509921
hamzadev2017 <wahlamuhammadhamza@gmail.com>
2025-01-21 15:42:30 +0500

    age checking
    this program lets you to see what age is allowed in 10th grade

---
+++ checkAge.py
@@ -0,0 +1,9 @@
+subzero = int(input("type your age :"))
+if subzero>20:
+    print("youre not allowed in 10th grade")
+elif subzero<10:
+    print("youre not allowed in 10th grade")
+else:
+    print("your allowed in 10th grade")
+
+        
